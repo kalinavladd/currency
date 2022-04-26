1ec6f98fc96567cd84d0a52dbf8ecd1707071d45
Vladyslav <97517741+kalinavladd@users.noreply.github.com>
2022-04-26 11:55:48 +0300

    Update settings.py

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'phantomassassinkill@gmail.com'
-EMAIL_HOST_PASSWORD = 'X4qq3a8w'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
